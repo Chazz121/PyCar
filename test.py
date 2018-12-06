b8f4680534ef78521bb2e2d91b3f279b0b2c61ec
cm <chrlsmitch123@gmail.com>
2018-12-06 16:08:32 -0500

    add server

--- test.py
+++ test.py
@@ -4,7 +4,21 @@ from picar import ADC
 import time
 import picar
 
-lf = Light_Follower.Light_Follower()
+HOST, PORT = "localhost", 9999
+data = " ".join(sys.argv[1:])
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(data + "\n")
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
+
 fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 adc = ADC()
